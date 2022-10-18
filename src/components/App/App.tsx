d452f994d84068a4f62a2ea549a25ab5e23b48b8
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-18 09:21:25 +0100

    finished parallax-ing of the title

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,38 @@
 import './app.scss';
-// import { useEffect } from 'react'; useTransform
-import { motion, useScroll, useSpring} from 'framer-motion';
-// import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
+import { motion, useScroll, useSpring, useInView, useTransform} from 'framer-motion';
+// import { gsap } from 'gsap'; 
 
 // import of assets
 import img0 from '../../assets/3.jpg'
 import img1 from '../../assets/1.jpg'
 import img2 from '../../assets/2.jpg'
+import img4 from '../../assets/4.jpg'
+import img5 from '../../assets/5.jpg'
+import img6 from '../../assets/6.jpg'
 
 
+// const t1 = 'S'.split('');
 const t1 = 'STANLEY'.split('');
+const t1Length = Math.floor(t1.length / 2) - 1
 const t2 = 'PARALLAX'.split('');
+const t2Length = Math.floor(t2.length / 2) - 1
 
-const TxtComp = ({item}: {item:string}) => {
-    return  <div className="">{item}</div>
+console.log({t1Length, t2Length})
+
+const TxtComp = ({item, speed}: {item:string, speed:number}) => {
+    const ref = useRef<HTMLDivElement>(null)
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        offset: ["0 end", "end start"]
+    })
+    const moveY = useTransform(scrollYProgress, [0, .5, 1], [0, 50, 100 * speed])
+
+    scrollYProgress.onChange(latest => {
+        console.log({latest, moveY: moveY.get()})
+    })
+
+    return  <motion.div className="" style={{y:moveY}} ref={ref}>{item}</motion.div>
 }
 
 const ImgComp = ({img}: {img:string}) => {
@@ -33,7 +52,7 @@ const App = () => {
             <div className="AppHero">
                 {
                     t1.map((item:string, index:number) => {
-                        return <TxtComp key={`${item}-${index}`} item={item} />
+                        return <TxtComp key={`${item}-${index}`} item={item} speed={index - t1Length} />
                     })
                 }
             </div>
@@ -45,9 +64,14 @@ const App = () => {
             <div className="AppHero">
                 {
                     t2.map((item:string, index:number) => {
-                        return <TxtComp key={`${item}-${index}`} item={item} />
+                        return <TxtComp key={`${item}-${index}`} item={item} speed={index - t2Length} />
                     })
                 }
+            </div>
+            <div className="AppImg">
+                <ImgComp img={img4} />
+                <ImgComp img={img5} />
+                <ImgComp img={img6} />
             </div>
         </div>
     )
