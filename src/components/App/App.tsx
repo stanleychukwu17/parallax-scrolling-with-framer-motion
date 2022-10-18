8c9b092f74c5b2416f2e9db25b8006b7d197376b
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-18 06:20:27 +0100

    started the design of this project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,26 @@ import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
 
+const t1 = 'Parallax'.split('');
+console.log(t1)
 const App = () => {
 
     return (
         <div className="AppMain">
-
+            <div className="AppHero">
+                {
+                    t1.map((item:string, index:number) => {
+                        return (
+                            <div className="" key={`${item}-${index}`}>{item}</div>
+                        )
+                    })
+                }
+            </div>
+            <div className="">
+                <div className=""><img src="" alt="" /></div>
+                <div className=""><img src="" alt="" /></div>
+                <div className=""><img src="" alt="" /></div>
+            </div>
         </div>
     )
 }
