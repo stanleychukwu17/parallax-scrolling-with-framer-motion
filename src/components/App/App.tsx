eee980167715555bf3e00ef82eec4678ce9da16e
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-18 06:57:33 +0100

    styled the indicator for page progress

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,19 @@ import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
 
-const t1 = 'Parallax'.split('');
+// import of assets
+import img0 from '../../assets/3.jpg'
+import img1 from '../../assets/1.jpg'
+import img2 from '../../assets/2.jpg'
+
+
+const t1 = 'STANLEY'.split('');
 console.log(t1)
 const App = () => {
 
     return (
         <div className="AppMain">
+            <div className="indicator"></div>
             <div className="AppHero">
                 {
                     t1.map((item:string, index:number) => {
@@ -17,10 +24,19 @@ const App = () => {
                     })
                 }
             </div>
-            <div className="">
-                <div className=""><img src="" alt="" /></div>
-                <div className=""><img src="" alt="" /></div>
-                <div className=""><img src="" alt="" /></div>
+            <div className="AppImg">
+                <div className=""><img src={img0} alt="" /></div>
+                <div className=""><img src={img1} alt="" /></div>
+                <div className=""><img src={img2} alt="" /></div>
+            </div>
+            <div className="AppHero">
+                {
+                    t1.map((item:string, index:number) => {
+                        return (
+                            <div className="" key={`${item}-${index}`}>{item}</div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
